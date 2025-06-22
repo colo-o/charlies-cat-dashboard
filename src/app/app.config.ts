18e36374679a7ebdac95797ade5c14b0a014b7ba
Steven Anaya <stvanayar@gmail.com>
2025-06-21 23:46:47 -0500

    fix: deployment configuration for productive environment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,14 @@
-import {
-  ApplicationConfig,
-  provideZonelessChangeDetection,
-} from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { errorInterceptor } from './core/interceptors/error.interceptor';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptors([errorInterceptor])),
-    provideAnimationsAsync(),
     provideZonelessChangeDetection(),
   ],
 };
